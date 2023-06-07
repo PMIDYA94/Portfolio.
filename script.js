137a27fcefa74dcb7de18cc29aa51e9e238ef92a
Pallab Midya <93614118+PMIDYA94@users.noreply.github.com>
2023-06-07 21:58:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-// https://harnishdesign.net/demo/react/simone/demo/?classicHeader=true&darkTheme=false&bgImage=true
+// ----------navbar menu toggle--------------
 
 document.querySelector('.menu').addEventListener('click', () => {
     document.querySelector('.menu').style.display = 'none';
     document.querySelector('.x').style.visibility = 'visible';
     document.querySelector('.navbar ul').classList.toggle('active');
 
-})
+});
 document.querySelector('.x').addEventListener('click', () => {
     document.querySelector('.menu').style.display = 'block';
     document.querySelector('.x').style.visibility = 'hidden';
     document.querySelector('.navbar ul').classList.remove('active');
-
-})
+});
+// -------type text-----------
+const typeText = new Typed('.type_text', {
+    strings: ['Front-end Developer', 'Fresher','Fast Lerner'],
+    typeSpeed: 50,
+    backSpeed:100,
+    backDelay:1000,
+    fadeOutClass: 'typed-fade-out',
+    loop:true
+  });
 
